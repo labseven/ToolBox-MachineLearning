ef6636459627003a599e2fcae2d7278e5be83263
Adam Novotny <adam.novotny@postrock.wall>
2017-03-20 17:41:29 -0400

    Change numpy to np, Complete train_model

--- learning_curve.py
+++ learning_curve.py
@@ -1,7 +1,7 @@
 """ Exploring learning curves for classification of handwritten digits """
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -13,25 +13,31 @@ def display_digits():
     fig = plt.figure()
     for i in range(10):
         subplot = fig.add_subplot(5, 2, i+1)
-        subplot.matshow(numpy.reshape(digits.data[i], (8, 8)), cmap='gray')
+        subplot.matshow(np.reshape(digits.data[i], (8, 8)), cmap='gray')
 
     plt.show()
 
 
 def train_model():
     data = load_digits()
-    num_trials = 10
-    train_percentages = range(5, 95, 5)
-    test_accuracies = numpy.zeros(len(train_percentages))
+    num_trials = 300
+    train_percentages = range(5, 95, 2)
+    test_accuracies = np.zeros(len(train_percentages))
 
-    # train models with training percentages between 5 and 90 (see
-    # train_percentages) and evaluate the resultant accuracy for each.
-    # You should repeat each training percentage num_trials times to smooth out
-    # variability.
-    # For consistency with the previous example use
-    # model = LogisticRegression(C=10**-10) for your learner
 
-    # TODO: your code here
+    for i in range(len(train_percentages)):
+        accuracy_of_trials = []
+
+        for j in range(num_trials):
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,
+            train_size=train_percentages[i])
+            model = LogisticRegression(C=10**-10)
+            model.fit(X_train, y_train)
+
+            accuracy_of_trials.append(model.score(X_train, y_train))
+
+        average_accuracy = np.average(accuracy_of_trials)
+        test_accuracies[i] = average_accuracy
 
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
@@ -41,6 +47,5 @@ def train_model():
 
 
 if __name__ == "__main__":
-    # Feel free to comment/uncomment as needed
-    display_digits()
-    # train_model()
+    # display_digits()
+    train_model()
