196f13429f86bbed054c6fe858411ebd753d2760
Adam Novotny <adam.novotny@postrock.wall>
2017-03-20 19:06:11 -0400

    Fixes on learning_curve.py

--- learning_curve.py
+++ learning_curve.py
@@ -20,8 +20,8 @@ def display_digits():
 
 def train_model():
     data = load_digits()
-    num_trials = 300
-    train_percentages = range(5, 95, 2)
+    num_trials = 100
+    train_percentages = range(5, 95, 5)
     test_accuracies = np.zeros(len(train_percentages))
 
 
@@ -30,13 +30,14 @@ def train_model():
 
         for j in range(num_trials):
             X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,
-            train_size=train_percentages[i])
+            train_size=train_percentages[i]*.01)
             model = LogisticRegression(C=10**-10)
             model.fit(X_train, y_train)
 
-            accuracy_of_trials.append(model.score(X_train, y_train))
+            accuracy_of_trials.append(model.score(X_train, y_train) * 100)
 
         average_accuracy = np.average(accuracy_of_trials)
+        print(str(train_percentages[i]) + ": " + str(average_accuracy))
         test_accuracies[i] = average_accuracy
 
     fig = plt.figure()
